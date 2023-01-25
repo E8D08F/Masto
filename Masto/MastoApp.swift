a49d2852fb918a312ed2a4b7d358b360aab46918
Toto Minai <the@unpopular.me>
2023-01-25 15:27:37 +0900

    Initial Commit

---
+++ Masto/MastoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastoApp.swift
+//  Masto
+//
+//  Created by Toto Minai on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MastoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
